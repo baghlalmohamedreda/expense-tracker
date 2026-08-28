868a3e915441f1e87e8cd47b8f51cae5d662cc91
Mohamed Reda Baghlal <baghlalmohamedreda.25@ump.ac.ma>
2026-08-28 17:24:46 +0100

    add bcrypt to  hash the password

--- backend/src/services/authService.js
+++ backend/src/services/authService.js
@@ -1,13 +1,13 @@
 import pool from "../config/db"
+import bcrypt from 'bcrypt'
 
 export async function createUser(user){
+    const hashedpassword =await bcrypt(user.password,10)
     const result= await pool.query(`insert into users(name,email,password) values ($1,$2,$3) 
         returning name,email,password`,
-        [user.name,user.email,user.password]
-
+        [user.name,user.email,hashedpassword]
 
     )
     return result.rows[0]
-
 
 }
